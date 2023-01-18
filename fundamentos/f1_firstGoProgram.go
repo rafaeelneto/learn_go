38d615ee9fa6bd421a3039f88c042d686f601f86
Rafael Gomes <5102522+rafaeelneto@users.noreply.github.com>
2023-01-18 16:38:23 -0300

    feat: write hello world with go

---
+++ fundamentos/f1_firstGoProgram.go
@@ -0,0 +1,15 @@
+// Executable programs start by the main package
+package main
+
+/*
+The codes in Go are organized in packages.
+In order to use them its need to use the import keyword
+It can be made import of one or multiple packages at the same time
+*/
+
+import "fmt"
+
+// The function main is the entry of a GO program
+func main(){
+	fmt.Println("Hello World")
+}
