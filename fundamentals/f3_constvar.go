af293b7f3edf37eaa4188bae442626bb9748898e
Rafael Neto <5102522+rafaeelneto@users.noreply.github.com>
2023-01-25 05:57:03 -0300

    fix: improve f3 notes

--- fundamentals/f3_constvar.go
+++ fundamentals/f3_constvar.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// Verbose way
 	const PI float64 = 3.123
@@ -10,5 +12,27 @@ func main() {
 
 	multiplyBy := 323
 
+	// unused variables generate a compile error
+	fmt.Println(raio, area, multiplyBy)
+
+	const (
+		a = 1
+		b = 2
+	)
+
+	var (
+		c = 1
+		d = 2
+	)
+
+	fmt.Println(c, d)
+
+
+	var e, f bool = true, false;
+
+	fmt.Println(e, f)
+
+	g, h, i := "string qualquer", false, 3.24
+	fmt.Println(g, h, i)
 	
 }
