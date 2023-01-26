403315aa2fbe99d0ecafcf1e267d4ade636345b2
Rafael Gomes <5102522+rafaeelneto@users.noreply.github.com>
2023-01-26 10:27:29 -0300

    fix: complete print commands lesson

--- fundamentals/f4_printCommands.go
+++ fundamentals/f4_printCommands.go
@@ -4,5 +4,28 @@ import "fmt"
 
 func main() {
 	fmt.Print("mesma")
-	fmt.print("linha")
+	fmt.Print("linha")
+
+	fmt.Println(" Nova")
+	fmt.Println("Linha")
+
+	x := 3.14984
+
+	// go doenst allow type coercion
+	// fmt.Println("o valor de x é " + x)
+
+	// ALTERNATIVAS
+	xs := fmt.Sprint(x)
+	fmt.Println(xs)
+	fmt.Println("o valor de x é", x, "!!!")
+
+	fmt.Printf("O valor de x formatado é %.2f", x)
+	
+	a := 1
+	b :=2.334432
+	c:= true
+	d:= "opa"
+
+	fmt.Printf("\n%d, %f, %.2f, %t, %s", a, b, b, c, d)
+	fmt.Printf("\n%v %v %v %v", a, b, c, d)
 }
