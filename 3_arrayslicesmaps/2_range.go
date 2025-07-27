e8abaf16d63a5964a4908d40897449d243581387
Rafael Gomes <5102522+rafaeelneto@users.noreply.github.com>
2025-07-27 11:13:12 -0300

    lesson 1

--- 3_arrayslicesmaps/2_range.go
+++ 3_arrayslicesmaps/2_range.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	numbers := [...]int{2, 4, 6, 10,505} // the three dots notations indicates that the compiler is going to count the numbers of elements on the array
+	numbers := [...]int{2, 4, 6, 10, 505} // the three dots notations indicates that the compiler is going to count the numbers of elements on the array
 
 	for i, number := range numbers {
 		fmt.Println(i, number)
@@ -11,6 +11,6 @@ func main() {
 
 	// _ ignores the index variable
 	for _, number := range numbers {
-		fmt.Println(	number)
+		fmt.Println(number)
 	}
 }
