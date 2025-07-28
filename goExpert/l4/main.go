ffa6689cc34f22a7cd98e6c96ca5766489306fd3
Rafael Gomes <5102522+rafaeelneto@users.noreply.github.com>
2025-07-28 20:17:47 -0300

    fmt

--- goExpert/l4/main.go
+++ goExpert/l4/main.go
@@ -9,4 +9,12 @@ var userId ID = 20
 func main() {
 
 	fmt.Printf("O tipo de userId é %T", userId) // main.ID
+
+	a := 1
+	b := 2.334432
+	c := true
+	d := "opa"
+
+	fmt.Printf("\n%d, %f, %.2f, %t, %s", a, b, b, c, d)
+	fmt.Printf("\n%v %v %v %v", a, b, c, d)
 }
